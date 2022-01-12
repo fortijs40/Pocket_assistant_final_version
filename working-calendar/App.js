dd1a172749b636508417b440dda82311b4c723e8
KristiansZ <95358251+KristiansZ@users.noreply.github.com>
2022-01-12 13:41:45 +0200

    Add files via upload

---
+++ working-calendar/App.js
@@ -0,0 +1,28 @@
+import { Calendar } from 'react-native-calendars';
+import { View } from 'react-native';
+import React from 'react';
+
+export default class Example extends React.Component {
+  render() {
+    return (
+      <View style={{ paddingTop: 50, flex: 1 }}>
+        <Calendar
+          current={'2022-01-01'}
+          minDate={'2012-05-10'}
+          maxDate={'2050-05-30'}
+          onDayPress={day => {
+            console.log('selected day', day);
+          }}
+          monthFormat={'yyyy MM'}
+          onMonthChange={month => {
+            console.log('month changed', month);
+          }}
+          hideArrows={false}
+          hideExtraDays={false}
+          disableMonthChange={false}
+          firstDay={1}
+        />
+      </View>
+    );
+  }
+}
