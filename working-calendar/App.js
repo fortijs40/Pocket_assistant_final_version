55a2786b2fa8dd581f12669c8df37a7f37ae4199
KristiansZ <95358251+KristiansZ@users.noreply.github.com>
2022-01-12 15:52:18 +0200

    Update App.js

--- working-calendar/App.js
+++ working-calendar/App.js
@@ -3,18 +3,20 @@ import { View } from 'react-native';
 import React from 'react';
 
 export default class WorkingCalendar extends React.Component {
+
   render() {
     return (
       <View style={{ paddingTop: 50, flex: 1 }}>
         <Calendar
+          style={{ marginTop: 150 }}
           current={'2022-01-01'}
           minDate={'2012-05-10'}
           maxDate={'2050-05-30'}
-          onDayPress={day => {
+          onDayPress={(day) => {
             console.log('selected day', day);
           }}
-          monthFormat={'yyyy MM'}
-          onMonthChange={month => {
+          monthFormat={'yyyy - MM'}
+          onMonthChange={(month) => {
             console.log('month changed', month);
           }}
           hideArrows={false}
